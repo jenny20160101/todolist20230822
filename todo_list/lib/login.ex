586924244d965783cea36cf67f4677a8ffd82275
jenny <jenny.hu@scrumcn.com>
2023-08-26 16:14:56 +0800

    wrong args for File.write

--- todo_list/lib/login.ex
+++ todo_list/lib/login.ex
@@ -19,12 +19,32 @@ defmodule Login do
     end
   end
 
-  defp save_updated_user(user) do
+  def logout() do
+    user = get_login_user()
+
+    if is_nil(user) do
+      {:error, "user not login"}
+    else
+      user = Map.put(user, :login, false)
+      save_updated_user(user)
+      {:ok, user}
+    end
+  end
+
+  defp get_login_user() do
     User.all_users()
-    |> Enum.reject(&(&1.name == user.name))
-    |> List.insert_at(0, user)
-    |> Jason.encode!()
-    |> File.write(@user_config_file_path)
+    |> IO.inspect(label: "all_users")
+    |> Enum.find(&(&1.login == true))
+  end
+
+  defp save_updated_user(user) do
+    file_content =
+      User.all_users()
+      |> Enum.reject(&(&1.name == user.name))
+      |> List.insert_at(0, user)
+      |> Jason.encode!()
+
+    :ok = File.write(@user_config_file_path, file_content)
   end
 
   def logout() do
